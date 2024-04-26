35454e5388aa4dadfd26414fbe457a718a250506
micheal081 <michig3000@gmail.com>
2024-04-26 15:07:45 +0100

    add projects

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,32 +1,63 @@
 export const Work = [
-	{
-		id: 1,
-		name: "URL SHORTENER",
-		description:
-			"I created a URL Shortening service that will help you in shortening your URL by which you can get a more precised version of your URL at ease.",
-		tags: ["NODE.JS", "TYPESCRIPT", "TYPEORM", "EXPRESSJS"],
+  {
+    id: 1,
+    name: "FINANCIAL MANAGEMENT SYSTEM WITH KEYCLOAK",
+    description:
+      "I created a financial management backend application, crafted with Node.js, Nest.js, and TypeScript, leverages Keycloak for robust security. It seamlessly handles financial operations, ensuring data integrity and confidentiality.",
+    tags: ["NODE.JS", "NESTJS", "TYPESCRIPT", "TYPEORM", "PRISMA", "MYSQL"],
 
-		demo: "https://github.com/micheal081/url-shortener-nodejs/",
-		github: "https://github.com/micheal081/url-shortener-nodejs",
-	},
-	{
-		id: 2,
-		name: "QR-CODE GENERATOR",
-		description:
-			"QR Code renderer is a dependency-free library to render QR Codes. The library makes it simple to integrate with any UI framework and comes with a prebuilt SVG renderer for the web",
-		tags: ["TYPESCRIPT", "JAVASCRIPT"],
+    demo: "https://github.com/micheal081/Financial_management_service_with_keycloak",
+    github:
+      "https://github.com/micheal081/Financial_management_service_with_keycloak",
+  },
+  {
+    id: 2,
+    name: "ARTICLE APP SYSTEM",
+    description:
+      "I created an article app backend system serves as the engine behind a content delivery application, managing the storage, retrieval, and manipulation of articles.",
+    tags: ["NODE.JS", "NESTJS", "TYPESCRIPT", "TYPEORM", "PRISMA", "MYSQL"],
 
-		demo: "https://github.com/micheal081/qr-code-generator/",
-		github: "https://github.com/micheal081/qr-code-generator",
-	},
-	{
-		id: 3,
-		name: "DOCKER-NODEJS TEMPLATE",
-		description:
-			"I created a docker-nodejs template ready for use by any developers or applications",
-		tags: ["NODE.JS", "TYPESCRIPT", "DOCKER", "MYSQL"],
+    demo: "https://github.com/micheal081/Article-app-system",
+    github: "https://github.com/micheal081/Article-app-system",
+  },
+  {
+    id: 3,
+    name: "	BANKING SERVICE APP",
+    description:
+      "I created a banking service application is a robust platform built with TypeScript, Node.js, and Nest.js, incorporating solid design principles like SOLID, DRY, and KISS.",
+    tags: ["NODE.JS", "TYPESCRIPT", "NESTJS", "POSTGRESQL"],
 
-		demo: "https://github.com/micheal081/docker-nodejs-template/",
-		github: "https://github.com/micheal081/docker-nodejs-template",
-	},
+    demo: "https://github.com/micheal081/Banking-service-app/",
+    github: "https://github.com/micheal081/Banking-service-app/",
+  },
+  {
+    id: 4,
+    name: "URL SHORTENER",
+    description:
+      "I created a URL Shortening service that will help you in shortening your URL by which you can get a more precised version of your URL at ease.",
+    tags: ["NODE.JS", "TYPESCRIPT", "TYPEORM", "EXPRESSJS"],
+
+    demo: "https://github.com/micheal081/url-shortener-nodejs/",
+    github: "https://github.com/micheal081/url-shortener-nodejs",
+  },
+  {
+    id: 5,
+    name: "QR-CODE GENERATOR",
+    description:
+      "QR Code renderer is a dependency-free library to render QR Codes. The library makes it simple to integrate with any UI framework and comes with a prebuilt SVG renderer for the web",
+    tags: ["TYPESCRIPT", "JAVASCRIPT"],
+
+    demo: "https://github.com/micheal081/qr-code-generator/",
+    github: "https://github.com/micheal081/qr-code-generator",
+  },
+  {
+    id: 6,
+    name: "DOCKER-NODEJS TEMPLATE",
+    description:
+      "I created a docker-nodejs template ready for use by any developers or applications",
+    tags: ["NODE.JS", "TYPESCRIPT", "DOCKER", "MYSQL"],
+
+    demo: "https://github.com/micheal081/docker-nodejs-template/",
+    github: "https://github.com/micheal081/docker-nodejs-template",
+  },
 ];
